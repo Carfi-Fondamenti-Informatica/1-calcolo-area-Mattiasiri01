43b197d26f78b570839de6279a919f62779b3784
Mattiasiri01 <100609727+Mattiasiri01@users.noreply.github.com>
2022-03-12 11:33:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,25 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
+  float a, b, c, Atriangolo=0, Aquadrato=0, Arettangolo=0, Atrapezio=0;
+  cout<< " La base del triangolo = a "<< " L'area del triangolo = b " <<endl;
+  cin>> a >> b;
+  cout<< " L'area del triangolo = ((a*b)/2) " <<endl;
+  cout<< ((a*b)/2) <<endl;
+  
+  cout<< " Il lato del quadrato = a "<<endl;
+  cin>> a;
+  cout<< " L'area del quadrato = (a*a) " <<endl;
+  cout<< (a*a) <<endl;
+  
+  cout<< " Il primo lato del rettangolo = a " << " Il secondo lato del rettangolo = b" <<endl;
+  cin>> a >> b;
+  cout<< " L'area del rettangolo = (a*b) "<<endl;
+  cout<< (a*b) <<endl;
+  
+  cout<< " La base minore del trapezio = a " << " La base maggiore del trapezio = b " << " L' altezza del trapezio = c <<endl;
+  cin>> a >> b >> c;
+  cout<< " L'area del trapezio = (((a+b)*c)/2) " <<endl;
+  cout<< (((a+b)*c)/2) <<endl;
 
 }
