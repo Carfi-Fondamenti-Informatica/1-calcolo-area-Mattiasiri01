fe9049c9cdf19f222d3be9b87a131ed2640f6852
Mattiasiri01 <100609727+Mattiasiri01@users.noreply.github.com>
2022-03-12 11:55:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,22 +5,22 @@ int main(){
   float a, b, c, Atriangolo=0, Aquadrato=0, Arettangolo=0, Atrapezio=0;
   cout<< " La base del triangolo = a "<< " L'altezza del triangolo = b " <<endl;
   cin>> a >> b;
-  cout<< " L'area del triangolo = ((a*b)/2) " <<endl;
+  cout<< " ((a*b)/2) " <<endl;
   cout<< ((a*b)/2) <<endl;
   
   cout<< " Il lato del quadrato = a "<<endl;
   cin>> a;
-  cout<< " L'area del quadrato = (a*a) " <<endl;
+  cout<< " (a*a) " <<endl;
   cout<< (a*a) <<endl;
   
   cout<< " Il primo lato del rettangolo = a " << " Il secondo lato del rettangolo = b" <<endl;
   cin>> a >> b;
-  cout<< " L'area del rettangolo = (a*b) "<<endl;
+  cout<< " (a*b) "<<endl;
   cout<< (a*b) <<endl;
   
   cout<< " La base minore del trapezio = a " << " La base maggiore del trapezio = b " << " L' altezza del trapezio = c" <<endl;
   cin>> a >> b >> c;
-  cout<< " L'area del trapezio = (((a+b)*c)/2) " <<endl;
+  cout<< " (((a+b)*c)/2) " <<endl;
   cout<< (((a+b)*c)/2) <<endl;
   
   return 0;
