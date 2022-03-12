fde9798a5e17e88bbf479a883405fd3e396251d1
Mattiasiri01 <100609727+Mattiasiri01@users.noreply.github.com>
2022-03-12 12:04:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,22 +5,18 @@ int main(){
   float a, b, c, Atriangolo=0, Aquadrato=0, Arettangolo=0, Atrapezio=0;
   cout<< " a "<< " b " <<endl;
   cin>> a >> b;
-  cout<< " ((a*b)/2) " <<endl;
   cout<< ((a*b)/2) <<endl;
   
   cout<< " a "<<endl;
   cin>> a;
-  cout<< " (a*a) " <<endl;
   cout<< (a*a) <<endl;
   
   cout<< " a " << " b " <<endl;
   cin>> a >> b;
-  cout<< " (a*b) "<<endl;
-  cout<< (a*b) <<endl;
+  cout<<  (a*b) <<endl;
   
   cout<< " a " << " b " << " c " <<endl;
   cin>> a >> b >> c;
-  cout<< " (((a+b)*c)/2) " <<endl;
   cout<< (((a+b)*c)/2) <<endl;
   
   return 0;
